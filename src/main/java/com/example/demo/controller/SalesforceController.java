098a9368df15c0b189aa47ceb1a7d5fbdd7bf03c
Mohamed/Anchors <mohamed@anchors-biz.com>
2025-11-20 09:18:05 +0900

    webhook test

--- src/main/java/com/example/demo/controller/SalesforceController.java
+++ src/main/java/com/example/demo/controller/SalesforceController.java
@@ -14,7 +14,7 @@ public class SalesforceController {
     private SalesforceApiService apiService;
 
     @PostMapping("/createAccount")
-    public Map createAccount() throws Exception {
+    public Map createAccount() throws Exception { // create account and return full details
         return apiService.createAndReturnFullAccount();
     }
 }
